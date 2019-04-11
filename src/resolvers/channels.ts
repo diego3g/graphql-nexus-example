486b4b11885d4c0d0c898ad8ea0d895839f4c44f
Diego Fernandes <diego.schell.f@gmail.com>
2019-04-10 22:14:22 -0300

    App setup

---
+++ src/resolvers/channels.ts
@@ -0,0 +1,20 @@
+import { queryType } from 'nexus';
+import { prismaObjectType } from 'nexus-prisma';
+
+import { Context } from '../types';
+
+export const Channel = prismaObjectType({
+  name: 'Channel',
+  definition(t) {
+    t.prismaFields(['*']);
+  },
+});
+
+export const Query = queryType({
+  definition(t) {
+    t.list.field('testChannels', {
+      type: 'Channel',
+      resolve: (parent, args, ctx: Context) => ctx.prisma.channels(),
+    });
+  },
+});
---
+++ src/server.ts
@@ -0,0 +1,37 @@
+import * as path from 'path';
+import { GraphQLServer } from 'graphql-yoga';
+import { makePrismaSchema } from 'nexus-prisma';
+import { prisma } from './generated/prisma-client';
+import datamodelInfo from './generated/nexus-prisma';
+import * as types from './resolvers';
+
+const schema = makePrismaSchema({
+  types,
+
+  prisma: {
+    datamodelInfo,
+    client: prisma,
+  },
+
+  outputs: {
+    schema: path.join(__dirname, './generated/schema.graphql'),
+    typegen: path.join(__dirname, './generated/nexus.ts'),
+  },
+
+  typegenAutoConfig: {
+    sources: [
+      {
+        source: path.join(__dirname, './types.ts'),
+        alias: 'types',
+      },
+    ],
+    contextType: 'types.Context',
+  },
+});
+
+const server = new GraphQLServer({
+  schema,
+  context: { prisma },
+});
+
+server.start();
